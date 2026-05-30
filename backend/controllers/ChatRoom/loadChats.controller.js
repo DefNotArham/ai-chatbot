6e9e787c6e646f177444a6b2551b9140fb7f9c6e
DefNotArham <arhamkabir231@gmail.com>
2026-05-30 19:09:08 -0400

    created load chats controller

---
+++ backend/controllers/ChatRoom/loadChats.controller.js
@@ -0,0 +1,25 @@
+import ChatRoom from "../../model/ChatRoom.model.js";
+
+const loadChatsController = async (req, res) => {
+  const { sessionId } = req.body;
+  try {
+    if (!sessionId)
+      return res
+        .status(400)
+        .json({ success: false, message: "SessionID required" });
+
+    const Chatrooms = await ChatRoom.find({
+      sessionId,
+    });
+
+    res.status(200).json({ success: true, Chatrooms });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
+
+export default loadChatsController;
