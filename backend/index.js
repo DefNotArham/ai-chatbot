bc46cfd4f89c3ce18a5799b56c87bfc113866eb0
DefNotArham <arhamkabir231@gmail.com>
2026-05-30 18:37:26 -0400

    created routes

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 
 import connectDb from "./db/connectDb.js";
+import ChatRoomRoutes from "./routes/ChatRoom.route.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
 connectDb();
+
+app.use(express.json());
+app.use("/Chatroom", ChatRoomRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
--- backend/routes/ChatRoom.route.js
+++ backend/routes/ChatRoom.route.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import createNewChatController from "../controllers/ChatRoom/createNewChat.controller.js";
+
 const router = express.Router();
 
 router.post("/create-newChat", createNewChatController);
