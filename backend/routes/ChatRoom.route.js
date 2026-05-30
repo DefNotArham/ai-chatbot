6a892eff990c6ffde0cb8ded0885e65d84619d29
DefNotArham <arhamkabir231@gmail.com>
2026-05-30 19:10:01 -0400

    created loadchats route

--- backend/routes/ChatRoom.route.js
+++ backend/routes/ChatRoom.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 import createNewChatController from "../controllers/ChatRoom/createNewChat.controller.js";
+import loadChatsController from "../controllers/ChatRoom/loadChats.controller.js";
 
 const router = express.Router();
 
+router.get("/load-chats", loadChatsController);
 router.post("/create-newChat", createNewChatController);
 
 export default router;
