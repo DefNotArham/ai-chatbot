7b64e82dc8c8b89d0d5509b02d2c2de0f86b9380
DefNotArham <arhamkabir231@gmail.com>
2026-06-17 21:22:57 -0400

    created input

--- frontend/src/components/NewChat.jsx
+++ frontend/src/components/NewChat.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 const NewChat = () => {
   return (
-    <div className="flex justify-center items-center w-[80%]">
-      <h1 className="text-3xl font-bold">How can I help you today?</h1>
+    <div className="flex flex-col justify-center items-center w-[80%]">
+      <h1 className="text-4xl font-bold">How can I help you today?</h1>
+      <div className="bg-[#1e293b] border border-white/10 rounded-3xl p-4 flex items-center gap-3 w-[80%] mt-5">
+        <textarea
+          placeholder="Ask anything..."
+          className="flex-1 bg-transparent resize-none border-none outline-none text-white placeholder:text-slate-400 min-h-7"
+        />
+
+        <button className="w-[42px] h-[42px] rounded-xl bg-blue-600 hover:bg-blue-700 transition text-white flex items-center justify-center cursor-pointer">
+          <FaArrowRight size={15} />
+        </button>
+      </div>
     </div>
   );
 };
