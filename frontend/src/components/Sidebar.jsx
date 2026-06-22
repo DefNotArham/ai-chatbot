63510c4ebdf11d3be73d7b8c88d33b0bcf7611ca
DefNotArham <arhamkabir231@gmail.com>
2026-06-21 21:39:52 -0400

    made it scrollible

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,44 +5,39 @@ import useChatroomStore from "../stores/Chatroom.store.js";
 
 const Sidebar = () => {
   const { ChatRooms, loadChats, createChatLoading } = useChatroomStore();
-
   useEffect(() => {
     const sessionId = localStorage.getItem("sessionId");
-
     if (sessionId) {
       loadChats(sessionId);
     }
   }, []);
-
   const navigate = useNavigate();
   const { chatroomId } = useParams();
 
   return (
-    <div className="bg-sidebar-background p-6 w-[20%]">
-      <h1 className="text-3xl font-bold">AI Assistant</h1>
-      <div>
+    <div className="bg-sidebar-background p-6 w-[20%] h-full flex flex-col">
+      <h1 className="text-3xl font-bold shrink-0">AI Assistant</h1>
+      <div className="flex flex-col flex-1 min-h-0">
         <button
           onClick={() => {
             navigate("/");
           }}
-          className={`bg-azure-blue flex items-center gap-1 text-sm w-full mt-6 h-10 px-3 rounded-lg font-bold cursor-pointer ${
+          className={`bg-azure-blue flex items-center gap-1 text-sm w-full mt-6 h-10 px-3 rounded-lg font-bold cursor-pointer shrink-0 ${
             !chatroomId ? "bg-azure-blue/85" : "bg-azure-blue"
           } `}
         >
           <FaPlus />
           New Chat
         </button>
-
-        <div className="flex flex-col gap-2 mt-5 w-full ">
+        <div className="flex flex-col gap-2 mt-5 w-full flex-1 overflow-y-auto min-h-0 custom-scrollbar">
           {createChatLoading && (
-            <div className="text-sm h-10 px-3 w-full rounded-lg bg-white/4 flex items-center">
+            <div className="text-sm h-10 px-3 w-full rounded-lg bg-white/4 flex items-center shrink-0">
               <span className="animate-pulse">Generating chat...</span>
             </div>
           )}
-
           {ChatRooms.map((c) => (
             <div
-              className={`text-sm h-10 px-3 w-full rounded-lg cursor-pointer flex items-center overflow-hidden ${
+              className={`text-sm h-10 px-3 w-full rounded-lg cursor-pointer flex items-center overflow-hidden shrink-0 ${
                 chatroomId === c?._id ? "bg-white/10" : "bg-white/4"
               }`}
               onClick={() => navigate(`/chatroom/${c?._id}`)}
@@ -56,5 +51,4 @@ const Sidebar = () => {
     </div>
   );
 };
-
 export default Sidebar;
